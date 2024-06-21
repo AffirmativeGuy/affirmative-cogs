4597066d9c8129468bfba1f80a18f00c7a277f38
Arnav singh <100131691+AffirmativeGuy@users.noreply.github.com>
2024-06-21 20:00:18 +0530

    Update cop.py

--- cop/cop.py
+++ cop/cop.py
@@ -71,8 +71,8 @@ class cop(commands.Cog):
 
 
 
-  @commands.command()
-  async def lboard(self, ctx):
+   @commands.command()
+   async def lboard(self, ctx):
      guild = ctx.guild
      msg = await bank.get_leaderboard(positions = 100, guild=guild)
      embed = discord.Embed(title = "Top 100", description = (f"{msg}"))
