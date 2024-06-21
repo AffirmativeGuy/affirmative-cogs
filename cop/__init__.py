7e794477e9a7041ccc29f3b694c4579098c62c4e
Arnav singh <100131691+AffirmativeGuy@users.noreply.github.com>
2024-06-21 20:35:31 +0530

    Update __init__.py

--- cop/__init__.py
+++ cop/__init__.py
@@ -1,14 +1,11 @@
 
-# Thanks to blizz-cogs, cause the core command remove part was copied from his simpleping cog.
+
 from .cop import cop
 
 
 async def setup(bot):
-    old_info_command = bot.get_command("info")
-    if old_info_command:
-        bot.remove_command("info")
-    await bot.add_cog(cop(bot, old_info_command))
+    bot.get_command("info")
+    bot.remove_command("info")
     bot.get_command("ping")
-    bot.get_command("inviteset")
     bot.remove_command("ping")
-    bot.remove_command("inviteset")
+    await bot.add_cog(cop())
