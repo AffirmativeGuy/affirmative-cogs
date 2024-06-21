96a718f86754d9a9dc2fbd7e1f94e33d9f1bcf50
Arnav singh <100131691+AffirmativeGuy@users.noreply.github.com>
2024-06-21 18:22:33 +0530

    Update cop.py

--- cop/cop.py
+++ cop/cop.py
@@ -73,7 +73,7 @@ class cop(commands.Cog):
 
     @commands.command()
     async def lboard(self, ctx, guild: discord.Guild = 1218038597882482788):
-        msg = await bank.get_leaderboard(1218038597882482788)
+        msg = await bank.get_leaderboard(guild: discord.Guild = 1218038597882482788)
       # msg = bank.get_leaderboard
         await ctx.send(msg)
          
