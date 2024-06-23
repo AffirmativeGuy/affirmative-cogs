f5613f7d83a101221eebc87a1016ae7f5838d00b
Arnav singh <100131691+AffirmativeGuy@users.noreply.github.com>
2024-06-23 09:58:51 +0530

    Update __init__.py

--- cop/__init__.py
+++ cop/__init__.py
@@ -4,10 +4,9 @@ from .cop import cop
 
 
 async def setup(bot):
-    old_ping_command = bot.get_command("ping")
     bot.get_command("info")
     bot.remove_command("info")
-    #bot.get_command("ping")
+    bot.get_command("ping")
     bot.remove_command("ping")
-    await bot.add_cog(cop(bot, old_ping_command))
+    await bot.add_cog(cop(bot))
   
