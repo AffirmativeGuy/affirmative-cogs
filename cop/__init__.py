7f2bc54da382d0b490d106196397dcc5c4bfe93e
Arnav singh <100131691+AffirmativeGuy@users.noreply.github.com>
2024-06-27 18:56:17 +0530

    Update __init__.py

--- cop/__init__.py
+++ cop/__init__.py
@@ -2,24 +2,10 @@ from redbot.core.bot import Red
 from .cop import cop
 
 
-'''async def setup(bot):
+async def setup(bot):
     bot.get_command("info")
     bot.remove_command("info")
     bot.get_command("ping")
     bot.remove_command("ping")
-    await bot.add_cog(cop(bot))'''
-async def cog_unload(self) -> None:
-    global old_ping
-    self.bot.remove_command("info")
-    if old_ping:
-          with contextlib.suppress(Exception):
-               self.bot.remove_command("ping")
-               self.bot.add_command(old_ping)
-async def setup(bot: Red) -> None:
-    global old_ping
-    old_ping = bot.get_command("ping")
-    if old_ping:
-        bot.remove_command(old_ping.name)
-        cog = cop(bot)
     await bot.add_cog(cop(bot))
-  
+
