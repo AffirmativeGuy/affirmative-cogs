4a613af057b5bdf477a7c95ef16260569faa26eb
AffirmativeGuy <israilser516@gmail.com>
2024-06-29 20:50:14 +0530

    Added command to remove invite cmd

--- cop/__init__.py
+++ cop/__init__.py
@@ -5,6 +5,8 @@ from .cop import cop
 async def setup(bot):
     bot.get_command("info")
     bot.remove_command("info")
+    bot.get_command("invite")
+    bot.remove_command("invite")
     bot.get_command("ping")
     bot.remove_command("ping")
     await bot.add_cog(cop(bot))
