5ffa1f82c7bb47c11992a891d47ca1be4a1578f0
AffirmativeGuy <israilser516@gmail.com>
2024-06-30 19:19:16 +0530

    Added prefix

--- cop/cop.py
+++ cop/cop.py
@@ -27,13 +27,13 @@ class cop(commands.Cog):
              self.bot.remove_command("invite")
              self.bot.remove_command("info")
              self.bot.remove_command("ping")
-    @commands.command(name="update")
+    @commands.command()
     @commands.is_owner()
     async def update(self, ctx: commands.Context, *cogs: InstalledCog):
         """Updates all the repo's!"""
         cog_update = self.bot.get_command("cog update")
         if cog_update is None:
-            return await.ctx.send(f"Load Zaa downloader cog by using `{ctx.clean_prefix}load downloader`!")
+            return await.ctx.send(f"The downloader cog is not loaded, load zaa downloader cog by using `m!load downloader`!")
         await ctx.invoke(cog_update, True, *cogs)
     @commands.command()
     async def info(self, ctx):
