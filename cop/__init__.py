4cdd1fc92cd1b66e83e1b7774247f14782be5d3b
AffirmativeGuy <israilser516@gmail.com>
2024-07-01 22:40:40 +0530

    hmm

--- cop/__init__.py
+++ cop/__init__.py
@@ -1,5 +1,24 @@
 from redbot.core.bot import Red
 from .cop import cop
 from .cop import setup
+import json
+from pathlib import Path
 
 
+
+old_info = None
+
+async def setup(bot: Red) -> None:
+    global old_info
+    old_info = bot.get_command("info")
+    if old_info:
+        bot.remove_command(old_info.name)
+    
+    cog = cop(bot)
+    await bot.add_cog(cog)
+
+def teardown(bot: Red) -> None:
+    global old_info
+    if old_info:
+        bot.remove_command("info")
+        bot.add_command(old_info)
