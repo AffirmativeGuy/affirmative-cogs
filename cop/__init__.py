ea1471dd5a8b9fbe437354d82a70f4f898b57628
AffirmativeGuy <israilser516@gmail.com>
2024-07-01 22:13:44 +0530

    uhhhh

--- cop/__init__.py
+++ cop/__init__.py
@@ -1,15 +1,5 @@
 from redbot.core.bot import Red
 from .cop import cop
+from .choose import setup
 
-
-async def setup(bot):
-    bot.get_command("inviteset")
-    bot.remove_command("inviteset")
-    bot.get_command("info")
-    bot.remove_command("info")
-    bot.get_command("invite")
-    bot.remove_command("invite")
-    bot.get_command("ping")
-    bot.remove_command("ping")
-    await bot.add_cog(cop(bot))
 
