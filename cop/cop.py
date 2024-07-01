448bf731bfbe77282b09b1ad0f03d43534c0324e
AffirmativeGuy <israilser516@gmail.com>
2024-07-01 22:47:08 +0530

    Removed useless code

--- cop/cop.py
+++ cop/cop.py
@@ -21,23 +21,15 @@ class cop(commands.Cog):
     """Some tools which gives info about Cop <:cop:1243924879045034075>."""
     def __init__(self, bot):
         self.bot = bot
-     #async def cog_unload(self) -> None:
-         #with contextlib.suppress(Exception):
-             #self.bot.get_command("info")
-    async def cog_unload(self):
-        global old_info
-        self.bot.get_command("ping")
-        self.bot.get_command("invite")
-        self.bot.remove_command("invite")
-        self.bot.remove_command("ping")
-
-        if old_info:
-            try:
-                self.bot.remove_command("info")
-            except:
-                pass
-            self.bot.add_command(old_info)
-    # Some part of the update command was taken from Vrt's pull command!
+    async def cog_unload(self) -> None:
+         with contextlib.suppress(Exception):
+             self.bot.get_command("info")
+             self.bot.get_command("ping")
+             self.bot.get_command("invite")
+             self.bot.remove_command("invite")
+             self.bot.remove_command("info")
+             self.bot.remove_command("ping")
+    # Some part of the update command was take from Vrt's pull command!
     @commands.command()
     @commands.is_owner()
     async def update(self, ctx: commands.Context, *cogs: InstalledCog):
@@ -103,16 +95,4 @@ class cop(commands.Cog):
         embed.set_image(url = 'https://media.discordapp.net/attachments/1251495443557519382/1256631212299124827/COPS_INVITE.png?ex=66817884&is=66802704&hm=33fd08ba819d0c6424bb0c2f44d536ac73eeedb9b0a6ebd923ac26967297e0e6&=&format=webp&quality=lossless&width=1025&height=342')
         await ctx.send(embed=embed)
 
-
-async def setup(bot: Red) -> None:
-    global old_info
-    old_info = bot.get_command("info")
-    if old_info:
-        bot.remove_command(old_info.name)
-
-    cog = cop(bot)
-
-    r = bot.add_cog(cog)
-    if r is not None:
-        await r
         
