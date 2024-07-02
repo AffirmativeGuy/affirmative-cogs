cf145a555504a4204158b2a09b7a635f1b133ea8
AffirmativeGuy <israilser516@gmail.com>
2024-07-02 13:54:16 +0530

    Added auto reload for old invite

--- cop/__init__.py
+++ cop/__init__.py
@@ -7,28 +7,30 @@ from pathlib import Path
 
 old_info = None
 old_ping = None
+old_invite = None
 
 # Thanks to yami for making me understand this code
 # I Will add the works of every line here in near future!
 async def setup(bot: Red) -> None: 
-    bot.get_command("invite")
-    bot.remove_command("invite")
+    global old_invite
     global old_info
     global old_ping
     old_info = bot.get_command("info")
     old_ping = bot.get_command("ping")
+    old_invite = bot.get_command("invite")
     if old_info:
         bot.remove_command(old_info.name)
     if old_ping:
         bot.remove_command(old_ping.name)
+    if old_invite:
+        bot.remove_command(old_invite.name)
     
     cog = cop(bot)
     await bot.add_cog(cog)
 
 # Thanks to yami for making me understand this code
 def teardown(bot: Red) -> None:
-    bot.get_command("invite")
-    bot.remove_command("invite")
+    global old_invite
     global old_info
     global old_ping
     if old_info:
@@ -37,3 +39,6 @@ def teardown(bot: Red) -> None:
     if old_ping:
         bot.remove_command("ping")
         bot.add_command(old_ping)
+    if old_invite:
+        bot.remove_command("invite")
+        bot.add_command(old_invite)
